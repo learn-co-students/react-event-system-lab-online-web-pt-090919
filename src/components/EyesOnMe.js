e689a76bc14591bb4d822e42a472aed9a399bf7f
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-19 02:54:06 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+export default class EyesOnMe extends Component {
+  
+  render(){
+     return (
+       <button onFocus={this.focusing}>Focus</button>
+       <button onBlur={this.bluring}>Blur</button>
+       )
+   }
+  bluring = () => {
+    console.log("Good!")
+  }
+  
+  focusing = () => {
+    console.log("Hey! Eyes on me!")
+  }
+  
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, {Component} from 'react'
+
+export default class Keypad extends Component {
+  render(){
+    return (
+      <input onKeyUp={this.enterPassword} type="password" />
+      )
+  }
+  
+  enterPassword = () => {
+    console.log('Entering password...')
+  }
+  
+  
+  
+}
